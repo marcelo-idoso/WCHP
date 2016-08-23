bfbdad0713e566df174776265ae6169d37460436
marcelo-idoso <marcelo_idoso@hotmail.com>
2016-08-23 15:39:47 -0300

    Inicio da Configuração do menu

--- index.php
+++ index.php
@@ -16,15 +16,24 @@ and open the template in the editor.
                 <div id="logo">
                     <img src="imagem/php_logo.png" alt="" />
                 </div>
-                
                 <div id="topo-right">
                     <p>Olá, Seja bem-Vindo <a href=""> Faça Login </a> ou <a href=""> Cadastre-se </a></p>
-                    
                     <form action="">
-                        <input type="text" name="buscar" id="buscar" required placeholder="pesquisar" />
+                        <input type="text" name="buscar" id="buscar" required placeholder="Pesquisar no Site... " />
                     </form>
                 </div>
             </div>
         </header>
+        <div id="menu">
+            <nav>
+                <ul id="menu-screen">
+                    <li>Categorias</li>
+                    <li>Empresa</li>
+                    <li>Parceiros</li>
+                    <li>Quem Somos</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+        </div>
     </body>
 </html>

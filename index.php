d80af40fc230b09c02f872533712ffac6f531863
marcelo-idoso <marcelo_idoso@hotmail.com>
2016-08-23 10:34:56 -0300

    Construção do Topo
    Foi concluido a configuração do Topo do WCHP Somente a parte HTML

--- index.php
+++ index.php
@@ -7,11 +7,24 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>WCHP</title>
+        <link rel="stylesheet" type="text/css" href="css/default.css" media="screen"/>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <header>
+            <div id="topo">
+                <div id="logo">
+                    <img src="imagem/php_logo.png" alt="" />
+                </div>
+                
+                <div id="topo-right">
+                    <p>Olá, Seja bem-Vindo <a href=""> Faça Login </a> ou <a href=""> Cadastre-se </a></p>
+                    
+                    <form action="">
+                        <input type="text" name="buscar" id="buscar" required placeholder="pesquisar" />
+                    </form>
+                </div>
+            </div>
+        </header>
     </body>
 </html>

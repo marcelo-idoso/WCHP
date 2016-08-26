ce91e3da89afdee3a7fe8b2af86880f2c91aaaa2
marcelo-idoso <marcelo_idoso@hotmail.com>
2016-08-26 16:38:18 -0300

    Inicio da resposive Header

--- index_bootstrap.php
+++ index_bootstrap.php
@@ -5,16 +5,17 @@
         <title>WCHP</title>
         <link rel="stylesheet" href="css/vendor/plugins.css" type="text/css" />
         <link rel="stylesheet" href="css/default.css" type="text/css" />
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
     </head>
     <body>
         <header>
             <div class="container" id="container"> <!-- -->
-                <div class="row" id="topo">
-                    <div class="col-md-2" id="logo">
+                <div id="topo">
+                    <div id="logo">
                         <img src="imagem/php_logo.png" class="img-responsive" alt="" />
                     </div>
-                    <div class="col-md-5 col-md-offset-4" id="pesquisa">
+                    <div id="pesquisa">
                         <p>Olá, Seja bem-Vindo <a href=""> Faça Login </a> ou <a href=""> Cadastre-se</a>. </p>
                         <form action="">
                             <div class="input-group">
@@ -30,5 +31,46 @@
                 </div> <!--Fim Topo -->
             </div> <!--Fim Container -->
         </header><!-- Fim da Header -->
+        <div id="menu_navegacao">
+            <div class="container">
+            <nav class="navbar navbar-default" id="menu_content">
+                <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Menu Categoria <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider" > </li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+        <li class="active"><a href="#">Link 1<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link 2</a></li>
+        <li><a href="#">Link 3</a></li>
+        <li><a href="#">Link 4</a></li>
+        
+        
+      </ul>
+
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+            </div></div>
     </body>
 </html>
